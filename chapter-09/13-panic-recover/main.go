7d9a16bb2eb650c1c79d580e4e9ad0316f710300
Khoa Le <leminkhoa@gmail.com>
2025-06-08 09:29:11 +0700

    Complete chapter 9

---
+++ chapter-09/13-panic-recover/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func div60(i int) {
+	defer func() {
+		if v := recover(); v != nil {
+			fmt.Println(v)
+		}
+	}()
+	fmt.Println(60 / i)
+}
+
+func main() {
+	for _, val := range []int{1, 2, 0, 6} {
+		div60(val)
+	}
+}
+
+/**
+Running this code produces the following output:
+60
+30
+runtime error: integer divide by zero
+10
+**/
